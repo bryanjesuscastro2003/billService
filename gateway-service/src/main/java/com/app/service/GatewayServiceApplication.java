4d025466cecd42d4f497ee017866b3d948c2b73b
bryanJesus2003 <jesusbryan155@gmail.com>
2023-10-26 16:09:19 -0600

    0409

--- gateway-service/src/main/java/com/app/service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/app/service/GatewayServiceApplication.java
@@ -1,12 +1,10 @@
 package com.app.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class GatewayServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DataSourceAutoConfiguration.class);
+        SpringApplication.run(GatewayServiceApplication.class, args);
     }
 }
