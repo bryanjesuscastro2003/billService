15ac6212d67f78f56b93f5b510cdc1efb8f942e8
bryanJesus2003 <jesusbryan155@gmail.com>
2023-10-26 12:14:56 -0600

    config-data

---
+++ config/src/test/java/com/configData/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configData.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
